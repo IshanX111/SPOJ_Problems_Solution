166c4681963739aa461fe18189d8f509e69c14ca
IshanX111 <51012063+IshanX111@users.noreply.github.com>
2020-02-12 01:44:55 +0600

    Add files via upload

---
+++ Life, the Universe, and Everything .cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int arr[99];
+int i;
+    for(i=0;;i++){
+        cin>>arr[i];
+        if(arr[i]==42){
+            break;
+        }
+        else
+        printf("%d\n",arr[i]);
+    }
+
+}
